1bfcea7cfca8aab9509c53b8726c481792ff0e81
DeniskaYT <_email>
2026-04-02 15:03:12 +0300

    V3 Black Ops: Registration and User List

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { ExpressPeerServer } = require('peer');
 
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-    path: '/myapp'
-});
-
+const peerServer = ExpressPeerServer(server, { debug: true, path: '/myapp' });
 app.use('/peerjs', peerServer);
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+let activeUsers = [];
 
 io.on('connection', (socket) => {
-    socket.on('join-room', (userId) => {
-        socket.broadcast.emit('user-connected', userId);
+    socket.on('register-user', (data) => {
+        socket.userId = data.peerId;
+        activeUsers.push({ id: data.peerId, name: data.name });
+        io.emit('update-user-list', activeUsers);
     });
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+
+    socket.on('join-voice-room', (userId) => {
+        socket.broadcast.emit('user-connected-voice', userId);
+    });
+
+    socket.on('chat message', (msg) => { io.emit('chat message', msg); });
+
+    socket.on('disconnect', () => {
+        activeUsers = activeUsers.filter(u => u.id !== socket.userId);
+        io.emit('update-user-list', activeUsers);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log('Бурмалдскорд с голосом на порту ' + PORT);
-});
+server.listen(PORT, () => { console.log('HQ LIVE ON PORT ' + PORT); });
