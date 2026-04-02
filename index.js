12a63791eb2ae61f201a3dba5dce84a7728b4dbd
DeniskaYT <_email>
2026-04-02 14:49:27 +0300

    Added peer for voice

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+﻿const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
 
-// Указываем, где будут лежать наши картинки и скрипты
+// Настройка PeerJS сервера для голосовых данных
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+    path: '/myapp'
+});
+
+app.use('/peerjs', peerServer);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -11,13 +18,18 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('Кто-то вошел в Бурмалдскорд!');
-    
+    // Когда кто-то заходит, выдаем ему ID для звонка
+    socket.on('join-room', (userId) => {
+        socket.broadcast.emit('user-connected', userId);
+    });
+
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg); // Рассылаем сообщение всем
+        io.emit('chat message', msg);
     });
 });
 
-server.listen(3000, () => {
-    console.log('Бурмалдскорд запущен на порту 3000');
+// ПОРТ: Render сам подставит нужный, или используем 3000 локально
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('Бурмалдскорд с голосом запущен на порту ' + PORT);
 });
