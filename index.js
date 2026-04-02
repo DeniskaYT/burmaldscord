1c0be8325cdd394e252d9afb0f2eb049658ea6be
DeniskaYT <_email>
2026-04-02 14:54:00 +0300

    Final voice fix with peer dependency

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { ExpressPeerServer } = require('peer');
 
-// Настройка PeerJS сервера для голосовых данных
 const peerServer = ExpressPeerServer(server, {
     debug: true,
     path: '/myapp'
@@ -18,18 +17,15 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    // Когда кто-то заходит, выдаем ему ID для звонка
     socket.on('join-room', (userId) => {
         socket.broadcast.emit('user-connected', userId);
     });
-
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     });
 });
 
-// ПОРТ: Render сам подставит нужный, или используем 3000 локально
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log('Бурмалдскорд с голосом запущен на порту ' + PORT);
+    console.log('Бурмалдскорд с голосом на порту ' + PORT);
 });
